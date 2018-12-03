ce1d7cdfd195ddd3e33ceb1499484eaecbf42b9c
wiliamsand <wiliamfs123@gmail.com>
2018-12-03 11:22:54 -0200

    Projeto concluído

--- resources/views/include/footer.blade.php
+++ resources/views/include/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="page-footer font-small blue">
 
   <!-- Copyright -->
-  <div class=" nav-link footer-copyright text-center py-3">© 2018 Desenvolvido por Ricardo, Wilian e Yuri
+  <div class=" nav-link footer-copyright text-center py-3">© 2018 Desenvolvido por Ricardo, Wiliam e Yuri
   </div>
   <!-- Copyright -->
 
